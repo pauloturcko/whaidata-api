05ca0487644c466e4c3d9e2421976713e292e1bf
Paulo Turco <pauloturcko@gmail.com>
2026-04-30 18:53:59 -0300

    feat: #0010 - Add Cors

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,36 @@
-import express from 'express';
-import {appDataSource} from './db/config/data-source';
-import 'dotenv/config';
-import {userRouter} from "./http/routes/user-routes";
-import {authRouter} from "./http/routes/auth-routes";
-import {cardRouter} from "./http/routes/card-routes";
-import {paymentMethodsRouter} from "./http/routes/payment-methods-routes";
-
+import express from "express";
+import cors from "cors";
+import { appDataSource } from "./db/config/data-source";
+import "dotenv/config";
+import { userRouter } from "./http/routes/user-routes";
+import { authRouter } from "./http/routes/auth-routes";
+import { cardRouter } from "./http/routes/card-routes";
+import { paymentMethodsRouter } from "./http/routes/payment-methods-routes";
 
 const app = express();
+app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({}));
 app.use(express.json());
 const PORT = process.env.PORT || 3000;
 
-app.use("/users", userRouter)
-app.use("/users", authRouter)
-app.use("/cards", cardRouter)
-app.use("/payment-method", paymentMethodsRouter)
+app.use("/users", userRouter);
+app.use("/users", authRouter);
+app.use("/cards", cardRouter);
+app.use("/payment-method", paymentMethodsRouter);
 
+appDataSource
+  .initialize()
+  .then(() => {
+    console.log("Database connected successfully!");
 
-appDataSource.initialize()
-    .then(() => {
-        console.log('Database connected successfully!');
-
-        app.get('/', (req, res) => {
-            res.send('DB Connected!');
-        });
-
-        app.listen(PORT, () => {
-            console.log(`Listening on port: ${PORT}`);
-        });
-    })
-    .catch((error) => {
-        console.error('Error connecting to the DB:', error);
+    app.get("/", (req, res) => {
+      res.send("DB Connected!");
     });
+
+    app.listen(PORT, () => {
+      console.log(`Listening on port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to the DB:", error);
+  });
