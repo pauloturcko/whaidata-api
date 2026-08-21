64f33b7e5a29571aad8dcefe43a0189c7cb7564c
pauloturcko <pauloturcko@gmail.com>
2026-08-21 17:56:49 -0300

    feat: #0017 → Add toggle endpoint for user payment preferences

--- src/http/routes/payment-methods-routes.ts
+++ src/http/routes/payment-methods-routes.ts
@@ -6,5 +6,6 @@ const paymentMethodsRouter = Router()
 const paymentMethodsController = new PaymentMethodsController()
 
 paymentMethodsRouter.get('/list', authMiddleware, (req, res) => paymentMethodsController.list(req, res));
+paymentMethodsRouter.patch("/:id/update-preferences", authMiddleware, (req, res) => paymentMethodsController.toggle(req, res))
 
 export {paymentMethodsRouter};
